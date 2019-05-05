1a63ff097651eafa31c14b42fe3642d7578a5520
Christian Drappi <christiandrappi@gmail.com>
2019-05-05 18:31:29 -0500

    card id maP

--- gin_utils/deck.py
+++ gin_utils/deck.py
@@ -3,9 +3,12 @@
 card_ranks = list('23456789TJQKA')
 card_suits = set('cdhs')
 
+cards_in_deck = [f'{r}{s}' for r in card_ranks for s in card_suits]
+
 card_choices = [(f'{rank}{suit}', f'{rank} of {suit}')
                 for rank in card_ranks for suit in card_suits]
 
+suit_ids = {'c': 0, 'd': 1, 'h': 2, 's': 3}
 card_values = {
     'A': 1,
     'T': 10,
@@ -13,4 +16,9 @@ card_values = {
     'Q': 12,
     'K': 13,
     **{d: int(d) for d in '23456789'}
+}
+
+card_id_map = {
+    f'{r}{s}': suit_ids[s] + (card_values[r] - 1) * 4
+    for r in card_ranks for s in card_suits
 }
