a873458bf5d11cbc9047468fc987daf22bf7ed0b
Christian Drappi <christiandrappi@gmail.com>
2019-05-05 20:28:48 -0500

    setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('requirements/common.txt') as f:
 
 setup(
     name='gin_utils',
-    version='2019.5.5.8',
+    version='2019.5.5.9',
     packages=['gin_utils'],
     install_requires=install_requires,
 )
