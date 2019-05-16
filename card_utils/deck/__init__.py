6a27389ad37e2b43cccef16376316f6f4efb19d2
Christian Drappi <christiandrappi@gmail.com>
2019-05-16 17:22:49 -0500

    move around deck utils a bit

--- card_utils/deck/__init__.py
+++ card_utils/deck/__init__.py
@@ -1,17 +1,20 @@
 """ constants to build the deck of cards and score the game """
 
-card_ranks = list('23456789TJQKA')
-card_suits = set('cdhs')
+ranks = list('23456789TJQKA')
+suits = set('cdhs')
 
-cards = [f'{r}{s}' for r in card_ranks for s in card_suits]
+cards = [f'{r}{s}' for r in ranks for s in suits]
 
 card_choices = [(f'{rank}{suit}', f'{rank} of {suit}')
                 for (rank, suit) in cards]
 
 suit_ids = {'c': 0, 'd': 1, 'h': 2, 's': 3}
-rank_ids = {rank: index for index, rank in enumerate(card_ranks)}
+rank_ids = {rank: index for index, rank in enumerate(ranks)}
 
-rank_values = {
+card_id_map = {card: index for index, card in enumerate(cards)}
+reverse_card_id_map = {index: card for card, index in card_id_map.items()}
+
+rank_to_values = {
     'A': 1,
     'T': 10,
     'J': 11,
@@ -19,7 +22,4 @@ rank_values = {
     'K': 13,
     **{d: int(d) for d in '23456789'}
 }
-value_to_rank = {14: 'A', **{v: r for r, v in rank_values.items()}}
-
-card_id_map = {card: index for index, card in enumerate(cards)}
-reverse_card_id_map = {index: card for card, index in card_id_map.items()}
+value_to_rank = {14: 'A', **{v: r for r, v in rank_to_values.items()}}
