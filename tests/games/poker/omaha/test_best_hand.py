0df3249c4c082889d3563bfcf1ec0f75c9653f42
Christian Drappi <christiandrappi@gmail.com>
2019-05-20 14:12:11 -0500

    cant find test...

--- tests/games/poker/omaha/test_best_hand.py
+++ tests/games/poker/omaha/test_best_hand.py
@@ -5,7 +5,7 @@ from card_utils.games.poker.omaha.utils import get_best_hand
 from tests.games.poker.util import deal_random_board_hands
 
 
-class BestOmahaHighHand(unittest.TestCase):
+class TestBestOmahaHighHand(unittest.TestCase):
     """ Test for the best Omaha high hand """
 
     n_random_cases = 100
@@ -15,6 +15,11 @@ class BestOmahaHighHand(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+    def test_random_cases(self):
+        for _ in range(self.n_random_cases):
+            board, hands = deal_random_board_hands(n_hands=8, n_cards=4)
+            self._test_best_hand(board, hands)
 
     def _test_best_hand(self, board, hands):
         """
@@ -32,8 +37,3 @@ class BestOmahaHighHand(unittest.TestCase):
                 f'Calculated: {hands[calculated_best_hand_index]}'
             )
         )
-
-    def test_random_cases(self):
-        for _ in range(self.n_random_cases):
-            board, hands = deal_random_board_hands(n_hands=8, n_cards=4)
-            self._test_best_hand(board, hands)
