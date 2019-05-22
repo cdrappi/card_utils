81a3078238062b5ae9c03d355cef2bf63bde71a9
Christian Drappi <christiandrappi@gmail.com>
2019-05-22 17:22:17 -0400

    struggling w varuable name, but chop logic seems correct

--- card_utils/games/poker/pot.py
+++ card_utils/games/poker/pot.py
@@ -9,7 +9,7 @@ class Pot:
         :param num_players: (int)
         """
         self.num_players = num_players
-        self.money_from_player = collections.Counter()
+        self.money_from = collections.Counter()
         self.pot = 0
 
     def put_money_in(self, player, amount):
@@ -17,7 +17,7 @@ class Pot:
         :param player: (int) index
         :param amount: (int) chips
         """
-        self.money_from_player[player] += amount
+        self.money_from[player] += amount
         self.pot += amount
 
     def settle_showdown(self, winning_players):
@@ -28,19 +28,19 @@ class Pot:
             with the strongest hand first
         :return: ({int: int}) player index --> amount won
         """
+        # TODO: test!
         payouts = {p: 0 for p in range(self.num_players)}
         for winners in winning_players:
-            # TODO: test!
-            chops_n_ways = len(winners)
+            n_chop = len(winners)
             for winner in winners:
-                max_winnings = self.money_from_player[winner]
+                amount_in = self.money_from[winner]
                 for p in range(self.num_players):
                     # can't win more than you risked
-                    winnings_from_p = min(self.money_from_player[p], max_winnings) / chops_n_ways
-                    payouts[winner] += winnings_from_p
-                    self.money_from_player[p] -= winnings_from_p
+                    from_p = min(self.money_from[p], amount_in) / n_chop
+                    payouts[winner] += from_p
+                    self.money_from[p] -= from_p
 
-            if sum(self.money_from_player.values()) == 0:
+            if sum(self.money_from.values()) == 0:
                 # we can terminate early when there's no money left
                 return payouts
 
