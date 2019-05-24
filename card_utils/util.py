754cff2e3781128b8f6b0a886c55b9f2cec2de23
Christian Drappi <christiandrappi@gmail.com>
2019-05-23 20:09:27 -0400

    inv cumsum

--- card_utils/util.py
+++ card_utils/util.py
@@ -1,5 +1,7 @@
 """ misc utils """
 
+from typing import List, Union
+
 
 def untuple_dict(the_dict):
     """ convert dict with keys that are tuples
@@ -17,3 +19,19 @@ def untuple_dict(the_dict):
         )
 
     return {to_key(k): v for k, v in the_dict.items()}
+
+
+def inverse_cumulative_sum(increasing_numbers: List[Union[int, float]]):
+    """ inverse function of cumulative sum
+
+    :param increasing_numbers: ([number]) [1,3,6]
+    :return: ([number]) [1,2,3]
+    """
+    if not increasing_numbers:
+        return []
+
+    inv_cumsum = [increasing_numbers[0]]
+    for e1, e2 in zip(increasing_numbers[:-1], increasing_numbers[1:]):
+        inv_cumsum.append(e2 - e1)
+
+    return inv_cumsum
