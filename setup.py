3c794e793a7137f9a42baa5775815aef0bdf0eaf
Christian Drappi <christiandrappi@gmail.com>
2019-05-31 23:05:36 -0400

    Suits as a list (#21)
    * make deck.suits a list so we preserve ordering
    
    * update setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('requirements/common.txt') as f:
 
 setup(
     name='card_utils',
-    version='2019.5.25.1',
+    version='2019.5.31',
     packages=['card_utils'],
     install_requires=install_requires,
 )
