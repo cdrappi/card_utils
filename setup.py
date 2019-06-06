97b8b393d3595928bac4e6aef8671bfb3342849c
Christian Drappi <christiandrappi@gmail.com>
2019-06-06 06:54:46 -0400

    plo pot sized bet

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('requirements/common.txt') as f:
 
 setup(
     name='card_utils',
-    version='2019.6.6',
+    version='2019.6.6.1',
     packages=['card_utils'],
     install_requires=install_requires,
 )
