d19cea9ad596aabc629e5e205d9ea7857b362e75
Christian Drappi <christiandrappi@gmail.com>
2019-06-29 04:21:35 -0400

    Community games: All in equity (#37)
    * all in rquity
    
    * update setup
    
    * fn

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('requirements/common.txt') as f:
 
 setup(
     name='card_utils',
-    version='2019.6.23',
+    version='2019.6.29',
     packages=['card_utils'],
     install_requires=install_requires,
 )
