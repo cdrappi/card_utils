fe380e51c1041e38ed90856a030e2e0cf7bf97ae
Christian Drappi <christiandrappi@gmail.com>
2020-06-17 17:54:18 -0700

    update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('requirements/common.txt') as f:
 
 setup(
     name='card_utils',
-    version='2020.5.9',
+    version='2020.6.17',
     packages=['card_utils'],
     install_requires=install_requires,
 )
