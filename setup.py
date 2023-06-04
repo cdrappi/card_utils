493e3d688476baadf39b03f7746860a2346a4a5f
Christian Drappi <christiandrappi@gmail.com>
2023-06-04 02:25:21 -0400

    pybind type stubs + build with pyproject.toml (#50)

--- setup.py
+++ setup.py
@@ -12,28 +12,14 @@ ext_modules = [
     ),
 ]
 
-
-def _is_install_requirement(requirement):
-    """return True iff setup should install requirement
-
-    :param requirement: (str) line of requirements.txt file
-    :return: (bool)
-    """
-    return not (requirement.startswith("-e") or "git+" in requirement)
-
-
-with open("requirements/common.txt") as f:
-    install_requires = [
-        requirement
-        for requirement in f.read().splitlines()
-        if _is_install_requirement(requirement)
-    ]
-
 setup(
     name="card_utils",
     version="2023.6.3",
-    packages=find_packages(),
-    install_requires=install_requires,
+    packages=[*find_packages(), "card_games"],
+    package_data={
+        "card_games": ["*.pyi"],
+    },
+    install_requires=[],
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
 )
