752413837a66e70383d2ee9c402672291fe24d93
Christian Drappi <christiandrappi@gmail.com>
2023-06-04 02:18:22 -0400

    OK

--- setup.py
+++ setup.py
@@ -12,23 +12,6 @@ ext_modules = [
     ),
 ]
 
-
-def _is_install_requirement(requirement):
-    """return True iff setup should install requirement
-
-    :param requirement: (str) line of requirements.txt file
-    :return: (bool)
-    """
-    return not (requirement.startswith("-e") or "git+" in requirement)
-
-
-with open("requirements/common.txt") as f:
-    install_requires = [
-        requirement
-        for requirement in f.read().splitlines()
-        if _is_install_requirement(requirement)
-    ]
-
 setup(
     name="card_utils",
     version="2023.6.3",
@@ -36,7 +19,7 @@ setup(
     package_data={
         "card_games": ["*.pyi"],
     },
-    install_requires=install_requires,
+    install_requires=[],
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
 )
