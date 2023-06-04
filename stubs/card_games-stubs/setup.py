2e265925c966e1e0ca463dc3a0d33b4936f0edcd
Christian Drappi <christiandrappi@gmail.com>
2023-06-04 01:49:46 -0400

    OK

--- stubs/card_games-stubs/setup.py
+++ stubs/card_games-stubs/setup.py
@@ -6,19 +6,19 @@ def find_stubs(package):
     stubs = []
     for root, dirs, files in os.walk(package):
         for file in files:
-            path = os.path.join(root, file).replace(package + os.sep, '', 1)
+            path = os.path.join(root, file).replace(package + os.sep, "", 1)
             stubs.append(path)
     return dict(package=stubs)
 
 
 setup(
-    name='card_games-stubs',
+    name="card_games-stubs",
     maintainer="card_games Developers",
     maintainer_email="example@python.org",
     description="PEP 561 type stubs for card_games",
-    version='1.0',
-    packages=['card_games-stubs'],
+    version="1.0",
+    packages=["card_games-stubs"],
     # PEP 561 requires these
-    install_requires=['card_games'],
-    package_data=find_stubs('card_games-stubs'),
+    install_requires=["card_games"],
+    package_data=find_stubs("card_games-stubs"),
 )
