1244ec624b8cd3d5e05aff218a23adbbd792e332
Christian Drappi <christiandrappi@gmail.com>
2023-06-04 01:37:13 -0400

    stubgen

--- stubs/card_games-stubs/__init__.pyi
+++ stubs/card_games-stubs/__init__.pyi
@@ -3,22 +3,17 @@ import card_games
 import typing
 
 __all__ = [
-    "add",
     "get_candidate_melds",
     "get_deadwood",
     "split_melds"
 ]
 
 
-def add(arg0: int, arg1: int) -> int:
-    """
-    A function which adds two numbers
-    """
-def get_candidate_melds(arg0: typing.List[str]) -> typing.List[typing.Tuple[int, typing.List[typing.List[str]], typing.List[str]]]:
+def get_candidate_melds(hand: typing.List[str]) -> typing.List[typing.Tuple[int, typing.List[typing.List[str]], typing.List[str]]]:
     """
     Get all candidate melds from list of cards
     """
-def get_deadwood(arg0: typing.List[str]) -> int:
+def get_deadwood(unmelded_cards: typing.List[str]) -> int:
     """
     Get deadwood from list of unmelded cards
     """
