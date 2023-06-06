8ff21aca3550d19be2d335b238d9fb9b0571a1ca
Christian Drappi <christiandrappi@gmail.com>
2023-06-06 00:45:30 -0400

    better

--- tests/gin/cpp_melds.py
+++ tests/gin/cpp_melds.py
@@ -2,7 +2,7 @@ from card_utils.games.gin.rummy.utils import deal_new_game, split_melds
 import card_games
 
 
-def _single_test() -> None:
+def _test_deadwood() -> None:
     new_game = deal_new_game()
     hand = new_game["p1_hand"]
 
@@ -15,8 +15,15 @@ def _single_test() -> None:
         raise ValueError("cpp_dw != py_dw")
 
 
+def test_layoffs() -> None:
+    hand = ["5c", "5d", "5h", "3s", "4s", "5s", "Ac", "Ad", "4h", "7s"]
+    opp_melds = [["3h", "3d", "3c"], ["7c", "7d", "7h"]]
+    print(card_games.layoff_deadwood(hand, opp_melds, True))
+
+
 if __name__ == "__main__":
-    for i in range(1_000_000):
-        _single_test()
-        if (i + 1) % 100_000 == 0:
-            print(f"thru {i+1} tests")
+    test_layoffs()
+    # for i in range(1_000_000):
+    #     _test_deadwood()
+    #     if (i + 1) % 100_000 == 0:
+    #         print(f"thru {i+1} tests")
