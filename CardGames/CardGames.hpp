5a5dc9d5ac026d1b8474f18b27d61a43722dc57c
Christian Drappi <christiandrappi@gmail.com>
2023-06-10 16:09:01 -0400

    hpP

---
+++ CardGames/CardGames.hpp
@@ -0,0 +1,14 @@
+#ifndef CARDGAMES_H
+#define CARDGAMES_H
+
+// #include "main.cpp"
+#include "src/deck/card.hpp"
+#include "src/deck/deck.hpp"
+#include "src/deck/rank.hpp"
+#include "src/deck/suit.hpp"
+#include "src/gin/gin_rummy.hpp"
+#include "src/gin/melds.hpp"
+#include "src/gin/gin_cards.hpp"
+#include "src/gin/gin_rummy.hpp"
+
+#endif // CARDGAMES_H
