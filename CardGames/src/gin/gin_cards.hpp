88829064ae2b72292cca26796d9c05ae83f2759f
Christian Drappi <christiandrappi@gmail.com>
2023-06-15 16:26:56 -0400

    OK

--- CardGames/src/gin/gin_cards.hpp
+++ CardGames/src/gin/gin_cards.hpp
@@ -13,6 +13,15 @@ public:
     Cards player2_hand;
     Cards discard_pile;
     Cards deck;
+    GinCards(
+        Cards player1_hand,
+        Cards player2_hand,
+        Cards discard_pile,
+        Cards deck)
+        : player1_hand(player1_hand),
+          player2_hand(player2_hand),
+          discard_pile(discard_pile),
+          deck(deck){};
 };
 
 GinCards DealHands(int n);
